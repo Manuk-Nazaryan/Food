e3191f861608dc22f3f770808a313753cc164853
Manuk-Nazaryan <143814201+Manuk-Nazaryan@users.noreply.github.com>
2023-09-01 17:26:17 +0400

    Add files via upload

---
+++ src.js
@@ -0,0 +1,71 @@
+// let name = prompt('մուտքագրեք ձեր անունը ')
+// let Lastname = prompt('մուտքագրեք ձեր ազգանունը')
+// alert('բարի գալուստ' + ' ' + name  + ' '+ Lastname)
+// // const toTopBtn = document.getElementById("to-top")
+// // const showTopBtn = () => {
+// //   if (window.scrollY > window.innerHeight) {
+// //     toTopBtn.style.right = "0"
+// //   } else {
+// //     toTopBtn.style.right = "-110px"
+// //   }
+// // }
+// // const smoothScroll = () => {
+// //   const scrolling = setInterval(() => {
+// //     if (window.scrollY > 0) {
+// //       scrollBy(0, -10)
+// //     } else {
+// //       clearInterval(scrolling)
+// //     }
+// //   }, 1)
+// // }
+// // window.addEventListener("scroll", showTopBtn)
+// // toTopBtn.addEventListener("click", (e) => {
+// //   e.preventDefault()
+// //   smoothScroll()
+// // })
+
+// const buy = document.getElementById('buy')
+// const buy1 = document.getElementById('buy1')
+// const buy2 = document.getElementById('buy2')
+// const buy3 = document.getElementById('buy3')
+// const buy4 = document.getElementById('buy4')
+// const buy5 = document.getElementById('buy5')
+// const buy6 = document.getElementById('buy6')
+// const buy7 = document.getElementById('buy7')
+// const buy8 = document.getElementById('buy8')
+// buy.addEventListener('click',()=>{
+//     alert('199$')
+//     alert('Շնորհակալություն գնումների համար')
+// })
+// buy1.addEventListener('click',()=>{
+//     alert('220$')
+//     alert('Շնորհակալություն գնումների համար')
+// })
+// buy2.addEventListener('click',()=>{
+//     alert('250$')
+//     alert('Շնորհակալություն գնումների համար')
+// })
+// buy3.addEventListener('click',()=>{
+//     alert('150$')
+//     alert('Շնորհակալություն գնումների համար')
+// })
+// buy4.addEventListener('click',()=>{
+//     alert('200$')
+//     alert('Շնորհակալություն գնումների համար')
+// })
+// buy5.addEventListener('click',()=>{
+//     alert('250$')
+//     alert('Շնորհակալություն գնումների համար')
+// })
+// buy6.addEventListener('click',()=>{
+//     alert('25$')
+//     alert('Շնորհակալություն գնումների համար')
+// })
+// buy7.addEventListener('click',()=>{
+//     alert('75$')
+//     alert('Շնորհակալություն գնումների համար')
+// })
+// buy8.addEventListener('click',()=>{
+//     alert('120$')
+//     alert('Շնորհակալություն գնումների համար')
+// })
